2abeb9cd7c29b3c49e03857e6610e4c0e549e304
Andres Felipe Tirano Vasquez <96450747+anfetirano@users.noreply.github.com>
2023-11-03 02:11:07 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,20 +2,37 @@ import React from "react";
 
 import "../../styles/home.css";
 
+
+
+
 export const Home = () => {
 
 	return (
 		<div className="container">
-			<div className="card mb-3 contact" style={{maxWidth: "540px"}}>
+			<button type="button" className="btn btn-success button">Add new contact</button>
+			{/* Contact Info bootstrap  */}
+			<div className="card mb-3 contact" style={{ maxWidth: "700px" }}>
 				<div className="row g-0">
 					<div className="col-md-4">
-						<img src="..." className="img-fluid rounded-start" alt="..."/>
+						<img src="https://cambiardeimagen.files.wordpress.com/2013/03/moda-masculina-lentes-cara-hombre-carametria-caramorfoligia-consultoria-de-imagen.jpg" className="img-fluid" alt="" />
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							<p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+							<h5 className="card-title name">Name</h5>
+							<div>
+								<i className="fas fa-edit mr-2"></i>
+								<i className="fas fa-trash-alt"></i>
+							</div>
+							<p className="card-text address">
+								<i className="fas fa-location-dot"></i> Address
+							</p>
+							<p className="card-text">
+								<i class="fa-solid fa-phone phone"></i> Phone
+							</p>
+							<p className="card-text">
+								<i class="fa-solid fa-envelope mail"></i> E-mail
+							</p>
+
 						</div>
 					</div>
 				</div>
